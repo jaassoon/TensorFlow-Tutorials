679d20984a156d5f7db7b114177ba2b48ed01ef3
jason <linglijunmail@gmail.com>
2017-12-03 21:55:36 +0900

    refact: print accuracy

--- CNN/face/train_faces.py
+++ CNN/face/train_faces.py
@@ -2,17 +2,24 @@ import tensorflow as tf
 # pip3 install opencv-python
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import os
 import random
 import sys
 from sklearn.model_selection import train_test_split
 
-my_faces_path = './my_faces'
-other_faces_path = './other_faces'
-size = 64
+my_faces_path = './my_faces1'
+my_faces_path = '/Users/jason/Documents/tensorTrain1112/data/train/cats'
+other_faces_path = '/Users/jason/Documents/tensorTrain1112/data/train/dogs'
+# other_faces_path = './other_faces1'
+# size = 256
+# size = 128
+# size = 64
+size = 32
+fc1Size=int(8*(size/64))
 
 imgs = []
-labs = []
+labels = []
 
 def getPaddingSize(img):
     h, w, _ = img.shape
@@ -45,15 +52,15 @@ def readData(path , h=size, w=size):
             img = cv2.resize(img, (h, w))
 
             imgs.append(img)
-            labs.append(path)
+            labels.append(path)
 
 readData(my_faces_path)
 readData(other_faces_path)
 # 将图片数据与标签转换成数组
 imgs = np.array(imgs)
-labs = np.array([[0,1] if lab == my_faces_path else [1,0] for lab in labs])
+labels = np.array([[0,1] if label == my_faces_path else [1,0] for label in labels])
 # 随机划分测试集与训练集
-train_x,test_x,train_y,test_y = train_test_split(imgs, labs, test_size=0.05, random_state=random.randint(0,100))
+train_x,test_x,train_y,test_y = train_test_split(imgs, labels, test_size=0.05, random_state=random.randint(0,100))
 # 参数：图片数据的总数，图片的高、宽、通道
 train_x = train_x.reshape(train_x.shape[0], size, size, 3)
 test_x = test_x.reshape(test_x.shape[0], size, size, 3)
@@ -63,7 +70,8 @@ test_x = test_x.astype('float32')/255.0
 
 print('train size:%s, test size:%s' % (len(train_x), len(test_x)))
 # 图片块，每次取100张图片
-batch_size = 100
+batch_size = 50
+# batch_size = 100
 num_batch = len(train_x) // batch_size
 
 x = tf.placeholder(tf.float32, [None, size, size, 3])
@@ -115,9 +123,11 @@ def cnnLayer():
     drop3 = dropout(pool3, keep_prob_5)
 
     # 全连接层 8*8矩阵64组；
-    Wf = weightVariable([8*8*64, 512])
+    Wf = weightVariable([fc1Size*fc1Size*64, 512])
+    # Wf = weightVariable([8*8*64, 512])
     bf = biasVariable([512])
-    drop3_flat = tf.reshape(drop3, [-1, 8*8*64])
+    drop3_flat = tf.reshape(drop3, [-1, fc1Size*fc1Size*64])
+    # drop3_flat = tf.reshape(drop3, [-1, 8*8*64])
     dense = tf.nn.relu(tf.matmul(drop3_flat, Wf) + bf)
     dropf = dropout(dense, keep_prob_75)
 
@@ -127,49 +137,50 @@ def cnnLayer():
     #out = tf.matmul(dropf, Wout) + bout
     out = tf.add(tf.matmul(dropf, Wout), bout)
     return out
+out = cnnLayer()
+# softmax:在分类结果出来之后，计算每个分类的概率。比如一批取32个样本，
+# 每个样本的结果保存在[0,1]类似的变量中，每个数组下标对应一个分类，
+# 这样在按数组下标来算概率的方法得到每个类别的概率分布
+y_pred = tf.nn.softmax(out)
+# arg Max：按行或列取该行或列最大值的下标，0表示按列，1表示按行；无论如何最终结果都是一行
+# 这里按行取，从而得到每个样本概率最大的值所在的下标
+# 形如[1 0 0 1 1 1 1 ...32个位置]
+y_pred_cls = tf.argmax(y_pred, dimension=1)
 
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=out,labels=y_)
+# reduce_mean：按指定维度求平均值，不指定维度，则求所有值的平均值，为一个数字
+cost = tf.reduce_mean(cross_entropy)
+# optimizer = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
+train_step = tf.train.AdamOptimizer(0.01).minimize(cost)
+# 比较标签是否相等，再求的所有数的平均值，tf.cast(强制转换类型)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), tf.argmax(y_, 1)), tf.float32))
+session = tf.Session()
+session.run(tf.global_variables_initializer())
 def cnnTrain():
-    out = cnnLayer()
-    # 交叉熵 
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=out, labels=y_))
-    
-    # AdamOptimizer可能优于RMSPropOptimizer算法 
-    train_step = tf.train.AdamOptimizer(0.01).minimize(cross_entropy)
-    # 比较标签是否相等，再求的所有数的平均值，tf.cast(强制转换类型)
-    accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(out, 1), tf.argmax(y_, 1)), tf.float32))
-    # 将loss与accuracy保存以供tensorboard使用
-    tf.summary.scalar('loss', cross_entropy)
-    tf.summary.scalar('accuracy', accuracy)
-    merged_summary_op = tf.summary.merge_all()
     # 数据保存器的初始化
     saver = tf.train.Saver()
-
-    with tf.Session() as sess:
-
-        sess.run(tf.global_variables_initializer())
-
-        summary_writer = tf.summary.FileWriter('./tmp', graph=tf.get_default_graph())
-
-        for n in range(10):
-             # 每次取128(batch_size)张图片
-            for i in range(num_batch):
-                batch_x = train_x[i*batch_size : (i+1)*batch_size]
-                batch_y = train_y[i*batch_size : (i+1)*batch_size]
-                # 开始训练数据，同时训练三个变量，返回三个数据
-                _,loss,summary = sess.run([train_step, cross_entropy, merged_summary_op],
-                                           feed_dict={x:batch_x,y_:batch_y, keep_prob_5:0.5,keep_prob_75:0.75})
-                summary_writer.add_summary(summary, n*num_batch+i)
-                # 打印损失
-                print(n*num_batch+i, loss)
-
-                if (n*num_batch+i) % 100 == 0:
-                    # 获取测试数据的准确率
-                    acc = accuracy.eval({x:test_x, y_:test_y, keep_prob_5:1.0, keep_prob_75:1.0})
-                    print(n*num_batch+i, acc)
-                    # 准确率大于0.98时保存并退出
-                    if acc > 0.98 and n > 2:
-                        saver.save(sess, './train_faces.model', global_step=n*num_batch+i)
-                        sys.exit(0)
-        print('accuracy less 0.98, exited!')
+    print('num_batch=',num_batch)
+    # 自定义迭代次数i
+    for n in range(1):
+    # for n in range(20):
+        for i in range(num_batch):
+            batch_x = train_x[i*batch_size : (i+1)*batch_size]
+            batch_y = train_y[i*batch_size : (i+1)*batch_size]
+            # 开始训练数据，同时训练三个变量，返回三个数据
+            _,loss = session.run([train_step, cost],
+                                       feed_dict={x:batch_x,y_:batch_y, keep_prob_5:0.5,keep_prob_75:0.75})
+            if i % 50 == 0:
+                # 获取测试数据的准确率
+                acc = accuracy.eval({x:test_x, y_:test_y, keep_prob_5:1.0, keep_prob_75:1.0},session=session)
+                print('i= ',i,'accuracy= ', acc)
+                feed_dict_train =    {x:batch_x,y_:batch_y, keep_prob_5:0.5,keep_prob_75:0.75}
+                feed_dict_validate = {x: test_x,y_: test_y, keep_prob_5:0.5,keep_prob_75:0.75}
+                acc = session.run(accuracy, feed_dict=feed_dict_train)
+                valid_acc = session.run(accuracy, feed_dict=feed_dict_validate)
+                msg = "Epoch {0} --- Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%}, Validation Loss: {3:.3f}"
+                print(msg.format(i, acc, valid_acc, loss))
+                saver.save(session, './model/train_faces.model', global_step=i)
 
 cnnTrain()
+saver = tf.train.Saver()
+
